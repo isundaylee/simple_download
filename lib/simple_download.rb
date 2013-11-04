f7e752079ae5fb6e44e451e5071517f77d8db5f7
Jiahao Li <isundaylee.reg@gmail.com>
2013-11-04 14:33:59 -0500

    'get' functionality.

--- lib/simple_download.rb
+++ lib/simple_download.rb
@@ -1,5 +1,38 @@
 require "simple_download/version"
 
+puts SimpleDownload.class
+
 module SimpleDownload
-  # Your code goes here...
+
+  require 'net/http'
+  require 'uri'
+
+  def self.get(url)
+    Thread.new do
+      thread = Thread.current
+      thread[:content] = ''
+      thread[:completed] = false
+      thread[:progress] = 0.0
+
+      partial = 0
+
+      uri = URI.parse url
+
+      Net::HTTP.new(uri.host, uri.port).request_get(uri.path) do |response|
+        thread[:length] = response['Content-Length'].to_i
+
+        response.read_body do |frag|
+          thread[:content] += frag
+          partial += frag.length
+          if thread[:length] > 0
+            thread[:progress] = partial.quo(thread[:length])
+          else
+            thread[:progress] = -1
+          end
+        end
+      end
+
+      thread[:completed] = true
+    end
+  end
 end
